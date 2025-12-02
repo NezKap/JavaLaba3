56d700d9d6e795d8f0a5f45f54abc195a5d103e2
NezKap <werras206@mail.ru>
2025-12-02 18:26:10 +0300

    Создан класс PefromanceComparison, а также создан класс Laba3Test

---
+++ src/test/java/org/example/Laba3Test.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class Laba3Test {
+
+    @Test
+    void testMethod() {
+
+    }
+}
